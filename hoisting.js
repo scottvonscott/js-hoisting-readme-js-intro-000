fd3009f88f06b3e669d5898e7405927baa05b632
Scott Keplinger <skeplin17@gmail.com>
2020-01-16 20:58:55 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -8,10 +8,10 @@ function callMe() {
 }
 
 
-function crazy() {
+function crazy()() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  } return function thisIsCrazy();
+  } thisIsCrazy();
 }
 
 function sayMyName() {
