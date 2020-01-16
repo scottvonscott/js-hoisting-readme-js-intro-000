021e773e55ba663c5b2cc9f8e71532d6d5416c9e
Scott Keplinger <skeplin17@gmail.com>
2020-01-16 20:07:08 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,5 +1,7 @@
+
 function callMe() {
-  var lyric = "maybe";
+  var lyric
+  lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
