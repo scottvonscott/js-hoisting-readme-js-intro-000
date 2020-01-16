0e0067a0e37e9784f8f752e3d447e7f4886ea566
Scott Keplinger <skeplin17@gmail.com>
2020-01-16 20:38:04 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,15 +1,8 @@
-
-
-function crazy() {
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  } return function thisIsCrazy();
-}
-
-function sayMyName() {
-  var name = "Cricky";
-  function sayMy() {
-   var name = "Kristin";
-    console.log(name);
-  } return function sayMy();
+function callMe() {
+  var lyric = "maybe";
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
 }
